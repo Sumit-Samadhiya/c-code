cbefbfa81644f3796dc977a0e6c83dc7255b1115
Sumit Samadhiya <121540749+Sumit-Samadhiya@users.noreply.github.com>
2022-12-28 22:46:59 +0530

    Add files via upload

---
+++ 10th- DO While loop..cpp
@@ -0,0 +1,17 @@
+//write a program print "ITM STUDENT" 30 times  by using dowhile loop.
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a=0;
+	do
+	{
+		
+		printf("  ITM STUDENT");
+		a++;
+		
+	}
+	while(a<30);
+	return 0;
+}
