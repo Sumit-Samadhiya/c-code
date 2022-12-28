fde4ac1423710e50f776602be897f7a6d2fdbb52
Sumit Samadhiya <121540749+Sumit-Samadhiya@users.noreply.github.com>
2022-12-28 20:52:12 +0530

    Add files via upload

---
+++ 2nd-  BASIC CALCULATION TWO NUMBER..cpp
@@ -0,0 +1,21 @@
+
+// BASIC CALCULATION.
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a,b;
+	printf("enter two value");
+	scanf("%d%d",&a,&b);
+	
+		printf("addion=%d\n",a+b);
+		
+		printf("subtraction=%d\n",a-b);
+		
+		printf("multipliction=%d\n",a*b);
+		
+		printf("division=%d\n",a/b);
+		
+		return 0;
+}
