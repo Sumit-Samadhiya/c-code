b3709ff7650f6dfc21e5f8ac704f6b636e637aec
Sumit Samadhiya <121540749+Sumit-Samadhiya@users.noreply.github.com>
2022-12-28 23:06:32 +0530

    Add files via upload

---
+++ A-- Do while loop infinite.cpp
@@ -0,0 +1,16 @@
+//write a program print "hello" infinite times  by using dowhile loop.
+
+#include<stdio.h>
+int main()
+{
+	int a;
+	do
+	{
+		a=0;
+		printf("  ITM STUDENT ");
+		
+		
+	}
+	while(1);
+	return 0;
+}
